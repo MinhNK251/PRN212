2e10fa870b860cccb21273c86af9721b0bf0241b
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-12-30 19:46:14 +0700

    Add Day 3 Project

---
+++ Day3/Bai1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            string result = "Invalid";
+            if (args.Length == 2)
+            {
+                try
+                {
+                    int num1 = Convert.ToInt32(args[0]);
+                    int num2 = Convert.ToInt32(args[1]);
+                    sum = num1 + num2;
+                    result = $"{num1} + {num2} = {sum}";
+                }
+                catch (FormatException)
+                {
+                    result = "Invalid";
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Day3/Bai2/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: count_directory <rootDirectory> <-l1|-all>");
+                return;
+            }
+            
+            string rootDirectory = args[0];
+            string option = args[1];
+
+            if (!Directory.Exists(rootDirectory))
+            {
+                Console.WriteLine("Folder not existed!");
+                return;
+            }
+
+            if (option != "-l1" && option != "-all")
+            {
+                Console.WriteLine("Invalid option! Usage: count_directory <rootDirectory> <-l1|-all>");
+                return;
+            }
+
+            int count = CountDirectories(rootDirectory, option);
+
+            if (option == "-l1")
+            {
+                Console.WriteLine($"Number of subdirectory level 1: {count}");
+            }
+            else if (option == "-all")
+            {
+                Console.WriteLine($"Total number of subdirectory: {count}");
+            }
+
+
+        }
+
+        static int CountDirectories(string directory, string option)
+        {
+            int count = 0;
+
+            string[] subDirectories = Directory.GetDirectories(directory);
+
+            foreach (string subDirectory in subDirectories)
+            {
+                if (option == "-l1")
+                {
+                    string[] subSubDirectories = Directory.GetDirectories(subDirectory);
+                    if (subSubDirectories.Length >= 0)
+                    {
+                        count++;
+                    }
+                }
+                else if (option == "-all")
+                {
+                    count++;
+                    count += CountDirectories(subDirectory, option);
+                }
+            }
+
+            return count;
+        }
+    }
+}
